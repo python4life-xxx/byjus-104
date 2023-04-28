91b2c57d1285191a6960712ce71a3f6e08997968
python4life-xxx <130096885+python4life-xxx@users.noreply.github.com>
2023-04-27 18:38:39 -0600

    fixed gamepage.js

--- gamepage.js
+++ gamepage.js
@@ -1,7 +1,26 @@
 // valor aleatorio generado
-  
+var y = Math.floor(Math.random() * 10 + 1)
 // contar el número de intentos
+var x= document.getElementById("guessField").value
 // crear para el intento correcto
-
+function submit(){
+	if(x == y){
+		alert("Correcto" + attempts "intento(s)")
+		attempts = 1
+	} else {
+		if (x < y){
+			alert("Incorrecto")
+			attempts++
+		} else {
+		  if (x > y){
+			 alert("Incorrecto")
+			 attempts++
+		  }
+		}
+	}
+}
   
 // función para el número adivinado por el usuario     
+function playAgain(){
+	y = Math.floor(Math.random() * 10 + 1)
+}
